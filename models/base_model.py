dd074a7579673b3a2f052915d445a5d5abd96a00
KevinFRX <xerokf13@gmail.com>
2022-06-27 10:52:40 -0700

    init, str & save

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
-"""Defines all common attributes/methods for other classes"""
+"""Module for BaseModel class"""
 
 
-class BaseModel
+import json
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.create_at = datetime.now()
+        self.update_at = datetime.now()
+
+    def __str__(self):
+        """Returns the string representation of the model"""
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates update_at with the current datetime"""
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        return self.__dict__
