aae3ceb0e4422a8a5c2673c9d3ee252026eccbbd
KevinFRX <xerokf13@gmail.com>
2022-06-28 11:20:02 -0700

    imports

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ point of the command interpreter"""
 
 import cmd, sys
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
