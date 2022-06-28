3db0c34f540d11f4381d995a2a8e7b71f9dcf0ed
KevinFRX <xerokf13@gmail.com>
2022-06-28 06:47:57 -0700

    storage val & FileStorage class

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 import json
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -27,6 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Returns the string representation of the model"""
@@ -36,6 +38,7 @@ class BaseModel:
     def save(self):
         """Updates update_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
---
+++ models/engine/file_storage.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module for FileStorage class"""
+
+
+import json
+import os
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        dctry = {}
+        for key in self.__objects:
+            dctry[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(dctry, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)"""
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path) as f:
+                objts = json.load(f)
+                for key, value in objts.items():
+                    self.new(eval[value['__class__']](**value))
